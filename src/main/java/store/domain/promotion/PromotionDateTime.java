6a2f661df659a8458a58879cd99d731230aeb3fe
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-06 15:26:03 +0900

    feat(Promotion, PromotionDateTime): 프로모션 관련 도메인 구현

---
+++ src/main/java/store/domain/promotion/PromotionDateTime.java
@@ -0,0 +1,14 @@
+package store.domain.promotion;
+
+public class PromotionDateTime {
+
+    private final String startDate;
+    private final String endDate;
+
+    public PromotionDateTime(final String startDate, final String endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+}
