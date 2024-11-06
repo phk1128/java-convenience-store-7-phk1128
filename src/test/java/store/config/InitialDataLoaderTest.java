1071d9aab9602abd06465b96fc98bce6a95d92f7
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-06 15:29:14 +0900

    test(InitialDataLoaderTest): 데이터 초기화 클래스 테스트

---
+++ src/test/java/store/config/InitialDataLoaderTest.java
@@ -0,0 +1,45 @@
+package store.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.product.Product;
+import store.domain.product.ProductRepository;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.PromotionRepository;
+
+class InitialDataLoaderTest {
+
+    private PromotionRepository promotionRepository;
+    private ProductRepository productRepository;
+    private InitialDataLoader initialDataLoader;
+
+    @BeforeEach
+    void setUp() {
+        promotionRepository = new PromotionRepository();
+        productRepository = new ProductRepository();
+        initialDataLoader = new InitialDataLoader(promotionRepository, productRepository);
+    }
+
+    @DisplayName("데이터 초기화를 성공한다.")
+    @Test
+    void initializeTest() {
+        //when
+        initialDataLoader.initialize();
+        final List<Promotion> promotions = promotionRepository.findAll();
+        final List<Product> products = productRepository.findAll();
+
+        //then
+        assertAll(
+                () -> assertThat(promotions).isNotEmpty(),
+                () -> assertThat(products).isNotEmpty(),
+                () -> assertThat(promotions).hasSize(3),
+                () -> assertThat(products).hasSize(16)
+        );
+
+    }
+}
