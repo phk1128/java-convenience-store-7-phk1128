47661ff99fdc09f51e196141b6eaba9f1089feca
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-06 15:27:17 +0900

    feat(Product): 상품 도메인 구현

---
+++ src/main/java/store/domain/product/Product.java
@@ -0,0 +1,37 @@
+package store.domain.product;
+
+import store.domain.promotion.Promotion;
+
+public class Product {
+
+    private final String name;
+    private final int price;
+    private final Promotion promotion;
+    private int quantity;
+
+    public Product(final String name, final int price, final Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.promotion = promotion;
+    }
+
+    public void updateQuantity(final int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
