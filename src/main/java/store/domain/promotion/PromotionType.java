c4f6f88c38fb18758e6defe6855b627d96fa3ce0
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-06 15:25:50 +0900

    feat(PromotionType): 프로모션 타입 정의

---
+++ src/main/java/store/domain/promotion/PromotionType.java
@@ -0,0 +1,30 @@
+package store.domain.promotion;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public enum PromotionType {
+
+    BUY_2_GET_1_FREE("탄산2+1"),
+    MD_PICK("MD추천상품"),
+    FLASH_DISCOUNT("반짝할인"),
+    NONE("");
+
+    private final String name;
+
+
+    PromotionType(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static PromotionType findByName(final String name) {
+        return Arrays.stream(PromotionType.values())
+                .filter(type -> Objects.equals(type.name, name))
+                .findAny()
+                .orElse(NONE);
+    }
+}
