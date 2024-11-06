0db28cb5b82638f36ea5f0720fdd0775d69e9aa5
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-06 15:26:41 +0900

    feat(PromotionRepository): 프로모션 레파지토리 구현

---
+++ src/main/java/store/domain/promotion/PromotionRepository.java
@@ -0,0 +1,27 @@
+package store.domain.promotion;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PromotionRepository {
+
+    private final Map<Long, Promotion> repository = new ConcurrentHashMap<>();
+
+    public void save(final Long id, final Promotion promotion) {
+        repository.put(id, promotion);
+    }
+
+    public Optional<Promotion> findByPromotionType(final PromotionType type) {
+        return repository.values().stream()
+                .filter(promotion -> Objects.equals(promotion.getType(), type))
+                .findAny();
+    }
+
+    public List<Promotion> findAll() {
+        return repository.values().stream()
+                .toList();
+    }
+}
