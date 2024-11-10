c44b6982c63e633fb4545f6bc29c10a6ccda0c90
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 17:00:31 +0900

    feat(PromotionController): 프로모션 관련 컨트롤러 레이어 구현
    구현한 내용은 다음과 같습니다:
    - 프로모션을 받을 수 있다면, 사용자에게 안내
    - 프로모션을 받을 수 없다면, 사용자에게 안내

---
+++ src/main/java/store/promotion/controller/PromotionController.java
@@ -0,0 +1,65 @@
+package store.promotion.controller;
+
+import java.util.List;
+import store.constant.StoreCommand;
+import store.product.domain.PurchaseProduct;
+import store.promotion.dto.NonPromotionProduct;
+import store.promotion.dto.PromotionProduct;
+import store.promotion.service.PromotionService;
+import store.state.PurchaseState;
+import store.util.LoopTemplate;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class PromotionController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final PromotionService promotionService;
+
+    public PromotionController(final InputView inputView, final OutputView outputView,
+                               final PromotionService promotionService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.promotionService = promotionService;
+    }
+
+    public void run() {
+        final List<PurchaseProduct> purchaseProducts = PurchaseState.getInstance().getPurchaseProducts();
+        for (PurchaseProduct product : purchaseProducts) {
+            checkPromotion(product);
+            checkNonPromotion(product);
+        }
+    }
+
+    private void checkPromotion(final PurchaseProduct product) {
+        final PromotionProduct promotionProduct = promotionService.getPromotionProduct(product);
+        if (promotionService.canReceiveMoreProduct(promotionProduct)) {
+            final StoreCommand storeCommand = requestPromotionStoreCommand(promotionProduct);
+            promotionService.applyPromotion(storeCommand, product);
+        }
+
+    }
+    private void checkNonPromotion(final PurchaseProduct product) {
+        final NonPromotionProduct nonPromotionProduct = promotionService.getNonPromotionProduct(product);
+        if (promotionService.canNotReceivePromotion(nonPromotionProduct)) {
+            final StoreCommand storeCommand = requestNonPromotionStoreCommand(nonPromotionProduct);
+            promotionService.applyNonPromotion(storeCommand, product);
+        }
+    }
+
+    private StoreCommand requestPromotionStoreCommand(final PromotionProduct promotionProduct) {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printGuidePromotionProduct(promotionProduct);
+            return inputView.readStoreCommand();
+        });
+    }
+
+    private StoreCommand requestNonPromotionStoreCommand(final NonPromotionProduct nonPromotionProduct) {
+        return LoopTemplate.tryCatchLoop(() -> {
+            outputView.printGuideNonPromotionProduct(nonPromotionProduct);
+            return inputView.readStoreCommand();
+        });
+    }
+
+}
