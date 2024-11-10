8ac3f77aa015653cdd849fd836f413b83da737db
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 17:23:35 +0900

    feat(AppConfig): 애플리케이션 관련 객체 생성 및 의존성 설정 클래스 구현

---
+++ src/main/java/store/config/AppConfig.java
@@ -0,0 +1,96 @@
+package store.config;
+
+import store.Convenience;
+import store.payment.controller.PaymentController;
+import store.payment.service.PaymentService;
+import store.product.controller.ProductController;
+import store.product.domain.ProductRepository;
+import store.product.service.ProductService;
+import store.product.service.PurchaseProductGenerator;
+import store.promotion.controller.PromotionController;
+import store.promotion.domain.PromotionRepository;
+import store.promotion.service.PromotionService;
+import store.view.console.ConsoleInputView;
+import store.view.console.ConsoleOutputView;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class AppConfig {
+
+    public Convenience convenience() {
+        return new Convenience(
+                productController(),
+                promotionController(),
+                paymentController()
+        );
+    }
+
+    public InitialDataLoader initialDataLoader() {
+        return new InitialDataLoader(
+                promotionRepository(),
+                productRepository()
+        );
+    }
+
+    private ProductRepository productRepository() {
+        return ProductRepository.getInstance();
+    }
+
+    private PromotionRepository promotionRepository() {
+        return PromotionRepository.getInstance();
+    }
+
+
+    private PurchaseProductGenerator purchaseProductGenerator() {
+        return new PurchaseProductGenerator(productRepository());
+    }
+
+    private ProductService productService() {
+        return new ProductService(
+                productRepository(),
+                purchaseProductGenerator()
+        );
+    }
+
+    private PromotionService promotionService() {
+        return new PromotionService(productRepository());
+    }
+
+    private PaymentService paymentService() {
+        return new PaymentService(productRepository());
+    }
+
+    private InputView inputView() {
+        return new ConsoleInputView();
+    }
+
+    private OutputView outputView() {
+        return new ConsoleOutputView();
+    }
+
+    private ProductController productController() {
+        return new ProductController(
+                inputView(),
+                outputView(),
+                productService()
+        );
+    }
+
+    private PromotionController promotionController() {
+        return new PromotionController(
+                inputView(),
+                outputView(),
+                promotionService()
+        );
+    }
+
+    private PaymentController paymentController() {
+        return new PaymentController(
+                inputView(),
+                outputView(),
+                paymentService()
+        );
+    }
+
+
+}
