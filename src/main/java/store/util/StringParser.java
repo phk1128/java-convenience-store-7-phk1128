3f06e83b3e45dd61baddee54f4592c5cd91787c3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 16:31:59 +0900

    refactor(StringParser): 문자열 특정 패턴 제거 추가 및 매직 넘버 상수화

--- src/main/java/store/util/StringParser.java
+++ src/main/java/store/util/StringParser.java
@@ -2,15 +2,24 @@ package store.util;
 
 import java.util.Arrays;
 import java.util.List;
+import store.error.ErrorMessage;
+import store.error.exception.InvalidWrongInputException;
 
 public class StringParser {
+
+    private static final String BLANK = "";
+    private static final int SPLIT_LIMIT = -1;
 
     private StringParser() {
 
     }
 
+    public static String removePattern(final String value, final String regex) {
+        return value.replaceAll(regex, BLANK);
+    }
+
     public static List<String> parseToTokens(final String value, final String delimiter) {
-        return Arrays.stream(value.split(delimiter, -1))
+        return Arrays.stream(value.split(delimiter, SPLIT_LIMIT))
                 .toList();
     }
 
