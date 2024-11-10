dfda7124843e4f59f471775234ef319bbe505375
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 16:27:51 +0900

    refactor(InitialDataLoader): 상품 객체 생성시 id 설정, 예외 처리 수정

--- src/main/java/store/config/InitialDataLoader.java
+++ src/main/java/store/config/InitialDataLoader.java
@@ -6,23 +6,26 @@ import java.nio.file.Paths;
 import java.util.ArrayDeque;
 import java.util.List;
 import java.util.Queue;
-import store.domain.product.Product;
-import store.domain.product.ProductRepository;
-import store.domain.promotion.Promotion;
-import store.domain.promotion.PromotionDateTime;
-import store.domain.promotion.PromotionRepository;
-import store.domain.promotion.PromotionType;
+import store.error.ErrorMessage;
+import store.error.exception.InvalidFailInitialDataLoadException;
+import store.product.domain.Product;
+import store.product.domain.ProductInfo;
+import store.product.domain.ProductRepository;
+import store.promotion.domain.Promotion;
+import store.promotion.domain.PromotionDateTime;
+import store.promotion.domain.PromotionRepository;
+import store.promotion.domain.PromotionType;
 import store.util.StringParser;
 
 public class InitialDataLoader {
 
-    private final static String PATH_PROMOTIONS = "promotions.md";
-    private final static String PATH_PRODUCTS = "products.md";
-    private final static String DELIMITER_COMMA = ",";
+    private static final String PATH_PROMOTIONS = "promotions.md";
+    private static final String PATH_PRODUCTS = "products.md";
+    private static final String DELIMITER_COMMA = ",";
     private final PromotionRepository promotionRepository;
     private final ProductRepository productRepository;
-    private Long PROMOTION_SEQUENCE = 1L;
-    private Long PRODUCT_SEQUENCE = 1L;
+    private Long promotionSequence = 1L;
+    private Long productSequence = 1L;
 
     public InitialDataLoader(final PromotionRepository promotionRepository, final ProductRepository productRepository) {
         this.promotionRepository = promotionRepository;
@@ -34,7 +37,6 @@ public class InitialDataLoader {
         saveAllProducts();
     }
 
-    //name,buy,get,start_date,end_date
     private void saveAllPromotions() {
         final Queue<String> fileLines = readFileLines(PATH_PROMOTIONS);
         if (fileLines.isEmpty()) {
@@ -47,7 +49,7 @@ public class InitialDataLoader {
         }
     }
 
-    //name,price,quantity,promotion
+
     private void saveAllProducts() {
         final Queue<String> fileLines = readFileLines(PATH_PRODUCTS);
         if (fileLines.isEmpty()) {
@@ -62,26 +64,26 @@ public class InitialDataLoader {
 
     private void saveProduct(final String line) {
         final List<String> tokens = StringParser.parseToTokens(line, DELIMITER_COMMA);
-        final Product product = createProduct(tokens);
-        productRepository.save(PRODUCT_SEQUENCE++, product);
+        final Long id = productSequence++;
+        final Product product = createProduct(id,tokens);
+        productRepository.save(id, product);
     }
 
-    private Product createProduct(final List<String> tokens) {
+    private Product createProduct(final Long id, final List<String> tokens) {
         final String promotionName = tokens.get(3);
         final Promotion promotion = promotionRepository.findByPromotionType(PromotionType.findByName(promotionName))
                 .orElse(null);
         final int price = StringParser.parseToNumber(tokens.get(1));
         final int quantity = StringParser.parseToNumber(tokens.get(2));
-        final Product product = new Product(tokens.get(0), price, promotion);
-        product.updateQuantity(quantity);
-        return product;
+        final ProductInfo productInfo = new ProductInfo(tokens.get(0), price, promotion);
+        return new Product(id, productInfo, quantity);
     }
 
 
     private void savePromotion(final String line) {
         final List<String> tokens = StringParser.parseToTokens(line, DELIMITER_COMMA);
         final Promotion promotion = createPromotion(tokens);
-        promotionRepository.save(PROMOTION_SEQUENCE++, promotion);
+        promotionRepository.save(promotionSequence++, promotion);
     }
 
     private Promotion createPromotion(final List<String> tokens) {
@@ -98,7 +100,7 @@ public class InitialDataLoader {
             URI uri = getClass().getClassLoader().getResource(path).toURI();
             return new ArrayDeque<>(Files.readAllLines(Paths.get(uri)));
         } catch (Exception e) {
-            throw new IllegalArgumentException("파일 읽기를 실패하였습니다. 어플리케이션 종료 후 다시 실행해주세요.");
+            throw new InvalidFailInitialDataLoadException(ErrorMessage.INVALID_FAIL_INITIAL_DATA_LOAD);
         }
     }
 }
