94a0e0d9f671bf6cb366a99e8f26b63201585333
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 17:24:50 +0900

    feat(Convenience): 메인 컨트롤러 레이어 구현

---
+++ src/main/java/store/Convenience.java
@@ -0,0 +1,32 @@
+package store;
+
+import store.payment.controller.PaymentController;
+import store.product.controller.ProductController;
+import store.promotion.controller.PromotionController;
+import store.state.PurchaseState;
+
+public class Convenience {
+
+    private final ProductController productController;
+    private final PromotionController promotionController;
+    private final PaymentController paymentController;
+
+    public Convenience(final ProductController productController, final PromotionController promotionController,
+                       final PaymentController paymentController) {
+        this.productController = productController;
+        this.promotionController = promotionController;
+        this.paymentController = paymentController;
+    }
+
+    public void purchase() {
+        do {
+            productController.run();
+            promotionController.run();
+            paymentController.run();
+        } while (isRetryPurchase());
+    }
+
+    private boolean isRetryPurchase() {
+        return PurchaseState.getInstance().isRetryPurchase();
+    }
+}
