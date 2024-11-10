03484ad8e986306fe02c0b3a04b0cd3041580f60
PARK HYUNG GYUN <qhk1198@Gmail.com>
2024-11-10 16:48:58 +0900

    feat(ProductService): 상품 관련 서비스 레이어 구현
    구현한 내용은 다음과 같습니다:
    - 구매 상품 요청 유효성 검증
    - 구매 상품 객체 생성
    - 현재 상품 재고 상태 dto 생성

---
+++ src/main/java/store/product/service/ProductService.java
@@ -0,0 +1,83 @@
+package store.product.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.error.ErrorMessage;
+import store.error.exception.ExceedsProductQuantityException;
+import store.error.exception.NotFoundProductException;
+import store.product.domain.Product;
+import store.product.domain.ProductInfo;
+import store.product.domain.ProductRepository;
+import store.product.domain.PurchaseProduct;
+import store.product.dto.ProductStatus;
+import store.product.dto.PurchaseProductRequest;
+import store.util.ListValidator;
+
+public class ProductService {
+
+    private static final int INTEGER_ZERO = 0;
+    private static final Long LONG_ZERO = 0L;
+    private final ProductRepository productRepository;
+    private final PurchaseProductGenerator purchaseProductGenerator;
+
+    public ProductService(final ProductRepository productRepository,
+                          final PurchaseProductGenerator purchaseProductGenerator) {
+        this.productRepository = productRepository;
+        this.purchaseProductGenerator = purchaseProductGenerator;
+    }
+
+    public List<PurchaseProduct> generatePurchaseProducts(final List<PurchaseProductRequest> requests) {
+        validate(requests);
+        return purchaseProductGenerator.createPurchaseProduct(requests);
+    }
+
+    public List<ProductStatus> getProductStatuses() {
+        final List<ProductStatus> productStatuses = new ArrayList<>();
+        final List<Product> products = productRepository.findAll();
+        for (Product product : products) {
+            final ProductStatus productStatus = ProductStatus.of(product);
+            productStatuses.add(productStatus);
+            addNormalProduct(product, productStatuses);
+        }
+        return productStatuses;
+    }
+
+    private void validate(final List<PurchaseProductRequest> requests) {
+        validateProductDuplicate(requests);
+        for (PurchaseProductRequest request : requests) {
+            validateProductName(request);
+            validateProductQuantity(request);
+        }
+    }
+
+    private void addNormalProduct(final Product product, final List<ProductStatus> productStatuses) {
+        if (hasNoNormalProduct(product)) {
+            final ProductInfo productInfo = new ProductInfo(product.getName(), product.getPrice(), null);
+            productStatuses.add(ProductStatus.of(new Product(LONG_ZERO, productInfo, INTEGER_ZERO)));
+        }
+    }
+
+    private boolean hasNoNormalProduct(final Product product) {
+        final String name = product.getName();
+        return productRepository.existPromotionProductByProductName(name)
+                && !productRepository.existNormalProductByProductName(name);
+    }
+
+    private void validateProductName(final PurchaseProductRequest request) {
+        if (!productRepository.existByProductName(request.name())) {
+            throw new NotFoundProductException(ErrorMessage.INVALID_NOT_FOUND_PRODUCT);
+        }
+    }
+
+    private void validateProductQuantity(final PurchaseProductRequest request) {
+        final int count = productRepository.countByProductName(request.name());
+        if (count < request.quantity()) {
+            throw new ExceedsProductQuantityException(ErrorMessage.INVALID_EXCEEDS_PRODUCT_QUANTITY);
+        }
+    }
+
+    private void validateProductDuplicate(final List<PurchaseProductRequest> requests) {
+        ListValidator.validateDuplicate(requests, ErrorMessage.INVALID_WRONG_INPUT);
+    }
+
+}
